22b984e475d7c8ea7c7e7c9d42d0c9b4b03706f6
Inyang1 <ediomoinyang@gmail.com>
2023-04-11 14:17:59 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,52 +1,64 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * strtow - Extract all the words from an string.
- * @str: String.
- *
- * Return: Array of words.
+ * strtow - concatenates all the arguments of your program
+ *@str: string
+ *@av: arguments
+ * Return: a pointer to a new string
  */
 char **strtow(char *str)
 {
+	int i, w, j, k, count, m, wordf;
+	char **p;
+	char *x;
 
-	int i = 0, j = 0, pos, t;
-	char **tmp;
-
-	if (str == NULL || _strcmp(str, "") || (words(str) == 0))
-	{
+	w = 0;
+	j = 0;
+	i = 0;
+	count = 0;
+	if (*str == '\0' || str == NULL)
 		return (NULL);
-	}
-	tmp = malloc(sizeof(int *) * (words(str) + 1));
-	if (tmp == NULL)
+	for (i = 0; str[i] != '\0'; i++)
 	{
+		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
+			w++;
+	}
+	p = (char **)malloc((w + 1) * sizeof(char *));
+	if (p == NULL)
 		return (NULL);
-	}
-	while (str[i])
+	for (wordf = 0; str[wordf] && j <= w; wordf++)
 	{
-		if (str[i] != ' ')
+		count = 0;
+		if (str[wordf] != ' ')
 		{
-			pos = _strlen(str, i);
-			tmp[j] = malloc(sizeof(char) * (pos + 1));
-			if (tmp[j] == NULL)
+			for (i = wordf ; str[i] != '\0'; i++)
 			{
-				for (t = j; t >= 0; t--)
+				if (str[i] == ' ')
+					break;
+				count++;
+			}
+			*(p + j) = (char *)malloc((count + 1) * sizeof(char));
+			if (*(p + j) == NULL)
+			{
+				for (k = 0; k <= j; k++)
 				{
-					free(tmp[t]);
+					x = p[k];
+					free(x);
 				}
-				free(tmp);
+				free(p);
 				return (NULL);
 			}
-			_strcpy(str, i, tmp[j]);
+			for (m = 0; wordf < i; wordf++)
+			{
+				p[j][m] = str[wordf];
+				m++;
+			}
+			p[j][m] = '\0';
 			j++;
-			i += pos;
-		}
-		else
-		{
-			i++;
 		}
 	}
-	tmp[j] = NULL;
-	return (tmp);
+	p[j] = NULL;
+	return (p);
 }
-
